a095c0edbf5653f184deda469a9850e28a6683cb
Nathaniel Schaaf <nschaaf@wikimedia.org>
2017-05-26 11:43:45 +0200

    Refactor

--- src/main/scala/TranslationRecommendations.scala
+++ src/main/scala/TranslationRecommendations.scala
@@ -1,59 +1,42 @@
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.{LabeledPoint, VectorIndexer}
 import org.apache.spark.ml.linalg.DenseVector
 import org.apache.spark.ml.regression.RandomForestRegressor
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-import scala.util.Random
-
 case class SitelinkPageviewsEntry(wikidataId: String, site: String, title: String, pageviews: Int)
 case class AggregateSitelinkPageviews(wikidataId: String, sitelinkCount: Int, pageviews: Int)
-case class WikidataItem(id: Int, sitelinks: Array[String])
-case class Features(sitelinkCount: Int, recentPageviews: Int)
+case class Features(wikidataId: String, sitelinkCount: Int, recentPageviews: Int)
 
 object TranslationRecommendations {
+  val FEATURES = "features"
+  val INDEXED_FEATURES = "indexedFeatures"
+  val LABEL = "label"
+  val PREDICTION = "prediction"
 
   def main(args: Array[String]): Unit = {
     val spark = initializeSpark()
 
-    val sitelinkPageviews = getSitelinkPageviews(spark).persist()
-    val preReduce = sitelinkPageviews.map(sitelinkPageviewsEntry => AggregateSitelinkPageviews(
-      sitelinkPageviewsEntry.wikidataId, 1, sitelinkPageviewsEntry.pageviews))
-
-    val grouped = preReduce.keyBy(item => item.wikidataId)
-    val aggregateSitelinkPageviews = grouped.reduceByKey((a, b) => AggregateSitelinkPageviews(a.wikidataId, a.sitelinkCount + b.sitelinkCount, a.pageviews + b.pageviews))
-    val features = aggregateSitelinkPageviews.map(item => Features(item._2.sitelinkCount, item._2.pageviews))
-
-//    val wikidataItems = getWikidataItems(spark)
-//    val features = wikidataItems.map(getFeatures)
-    val rankedFeatures = rankFeatures(features)
-
-    val data = prepareDataFrame(spark, rankedFeatures)
-
-    val featureIndexer = new VectorIndexer()
-      .setInputCol("features")
-      .setOutputCol("indexedFeatures")
-      .setMaxCategories(2)
-      .fit(data)
+    val sitelinkPageviewsEntries = getSitelinkPageviewsEntries(spark).persist()
+    val data = transformEntriesToFeatures(spark, sitelinkPageviewsEntries)
 
     val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
 
     val regressor = new RandomForestRegressor()
-      .setLabelCol("label")
-      .setFeaturesCol("indexedFeatures")
+      .setLabelCol(LABEL)
+      .setFeaturesCol(FEATURES)
       .setNumTrees(50)
 
-    val pipeline = new Pipeline().setStages(Array(featureIndexer, regressor))
+    val pipeline = new Pipeline().setStages(Array(regressor))
     val model = pipeline.fit(trainingData)
     val predictions = model.transform(testData)
 
-    predictions.select("prediction", "label", "features").show(500)
+    predictions.show(500)
 
     val evaluator = new RegressionEvaluator()
-      .setLabelCol("label")
-      .setPredictionCol("prediction")
+      .setLabelCol(LABEL)
+      .setPredictionCol(PREDICTION)
       .setMetricName("rmse")
     val rmse = evaluator.evaluate(predictions)
     println("Root Mean Squared Error (RMSE) on test data = " + rmse)
@@ -69,31 +52,28 @@ object TranslationRecommendations {
       .getOrCreate()
   }
 
-  def getSitelinkPageviews(spark: SparkSession): RDD[SitelinkPageviewsEntry] = {
+  def getSitelinkPageviewsEntries(spark: SparkSession): RDD[SitelinkPageviewsEntry] = {
     spark.read
       .format("csv")
       .option("header", "true")
       .option("mode", "DROPMALFORMED")
-      .csv("sitelink-pageviews.csv")
+      .csv("lite-sitelink-pageviews.csv")
       .rdd
       .map(row => SitelinkPageviewsEntry(row.getString(1), row.getString(2), row.getString(3), row.getString(4).toInt))
   }
 
-  def getWikidataItems(spark: SparkSession): RDD[WikidataItem] = {
-    val wikidataIds = (80 to 200).toArray
-    val ids = spark.sparkContext.parallelize(wikidataIds)
-    ids.map(id => WikidataItem(id, getSitelinks(id)))
-  }
+  def transformEntriesToFeatures(spark: SparkSession, entries: RDD[SitelinkPageviewsEntry]): DataFrame = {
+    val preReduce = entries.map(sitelinkPageviewsEntry => AggregateSitelinkPageviews(
+      sitelinkPageviewsEntry.wikidataId, 1, sitelinkPageviewsEntry.pageviews))
 
-  def getSitelinks(wikidataId: Int): Array[String] = {
-    Array("en", "de")
-  }
+    val grouped = preReduce.keyBy(item => item.wikidataId)
+    val aggregateSitelinkPageviews = grouped.reduceByKey((a, b) =>
+      AggregateSitelinkPageviews(a.wikidataId, a.sitelinkCount + b.sitelinkCount, a.pageviews + b.pageviews))
+    val features = aggregateSitelinkPageviews.map(item => Features(item._1, item._2.sitelinkCount, item._2.pageviews))
 
-  def getFeatures(wikidataItem: WikidataItem): Features = {
-    Features(
-      sitelinkCount=wikidataItem.sitelinks.length,
-      recentPageviews=Random.nextInt(10000)
-    )
+    val rankedFeatures = rankFeatures(features)
+
+    prepareDataFrame(spark, rankedFeatures)
   }
 
   def rankFeatures(features: RDD[Features]): RDD[(Features, Double)] = {
@@ -111,13 +91,8 @@ object TranslationRecommendations {
   }
 
   def prepareDataFrame(spark: SparkSession, rankedFeatures: RDD[(Features, Double)]): DataFrame = {
-    val labeled = rankedFeatures.map(getLabeled)
-    spark.createDataFrame(labeled).toDF("label", "features")
-  }
-
-  def getLabeled(rankedFeature: (Features, Double)): LabeledPoint = {
-    val (features, rank) = rankedFeature
-    LabeledPoint(rank, getFeatureVector(features))
+    val labeled = rankedFeatures.map{case (features, rank) => (features.wikidataId, rank, getFeatureVector(features))}
+    spark.createDataFrame(labeled).toDF("id", LABEL, FEATURES)
   }
 
   def getFeatureVector(features: Features): DenseVector = {
